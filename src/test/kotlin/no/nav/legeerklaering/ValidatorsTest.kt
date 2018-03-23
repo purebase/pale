dd9f3d296f40181aa7ba8f73bc075910349f3013
Kartveit <joakim.kartveit@nav.no>
2018-03-23 16:58:13 +0100

    la til test av fnr

---
+++ src/test/kotlin/no/nav/legeerklaering/ValidatorsTest.kt
@@ -0,0 +1,13 @@
+package no.nav.legeerklaering
+
+import org.junit.Assert.*
+import org.junit.Test
+
+class ValidatorsTest {
+
+    @Test
+    fun testSomething() {
+
+        assertTrue(validatePersonNumber("30063104414"))
+    }
+}
