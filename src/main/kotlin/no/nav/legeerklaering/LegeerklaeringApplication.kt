2f91e59129b724e4bb3ae84e56c33094331a2cfb
Kartveit <joakim.kartveit@nav.no>
2018-04-04 12:22:33 +0200

    La til loggik for å lage ein OK APPREC

--- src/main/kotlin/no/nav/legeerklaering/LegeerklaeringApplication.kt
+++ src/main/kotlin/no/nav/legeerklaering/LegeerklaeringApplication.kt
@@ -5,15 +5,18 @@ import com.fasterxml.jackson.module.jaxb.JaxbAnnotationModule
 import com.ibm.mq.jms.MQConnectionFactory
 import com.ibm.msg.client.wmq.WMQConstants
 import com.ibm.msg.client.wmq.compat.base.internal.MQC
-import no.nav.model.fellesformat.AppRec
+import no.nav.model.fellesformat.*
 import no.nav.model.legeerklaering.Legeerklaring
 import no.nav.tjeneste.virksomhet.person.v3.HentPerson
+import java.util.*
 import javax.jms.BytesMessage
 import javax.jms.MessageConsumer
-import javax.xml.datatype.XMLGregorianCalendar
+import javax.xml.datatype.DatatypeFactory
+
 
 val jaxbAnnotationModule = JaxbAnnotationModule()
 val objectMapper: ObjectMapper = ObjectMapper().registerModule(jaxbAnnotationModule)
+val newInstance = DatatypeFactory.newInstance()
 
 fun main(args: Array<String>) {
     val fasitProperties = FasitProperties()
@@ -73,14 +76,96 @@ fun connectionFactory(fasitProperties: FasitProperties) = MQConnectionFactory().
     setIntProperty(WMQConstants.JMS_IBM_CHARACTER_SET, 1208)
 }
 
-fun createApprec(legeeklaering: Legeerklaring, gendate: XMLGregorianCalendar): String {
+fun createApprec(fellesformat: EIFellesformat): String {
 
-      val apprec = AppRec().apply {
-          msgType.v = "APPREC"
-          miGversion = "1.0 2004-11-21"
-          genDate = gendate
-      }
+    val fellesformatApprec = EIFellesformat().apply {
+        mottakenhetBlokk = mottakenhetBlokk.apply {
+            ediLoggId = fellesformat.mottakenhetBlokk.ediLoggId
+            ebRole = LegeerklaeringConstant.ebRoleNav.string
+            ebService = LegeerklaeringConstant.ebServiceLegemelding.string
+            ebAction = LegeerklaeringConstant.ebActionSvarmelding.string
+        }
+        AppRec().apply {
+            msgType.v = LegeerklaeringConstant.APPREC.string
+            miGversion = LegeerklaeringConstant.APPRECVersionV1_0.string
+            genDate = newInstance.newXMLGregorianCalendar(GregorianCalendar())
+            id = fellesformat.mottakenhetBlokk.ediLoggId
+
+            sender.hcp.inst.apply {
+                name = fellesformat.msgHead.msgInfo.receiver.organisation.organisationName
+
+                for (i in fellesformat.msgHead.msgInfo.receiver.organisation.ident.indices) {
+                    id = mapIdentToInst(fellesformat.msgHead.msgInfo.receiver.organisation.ident.first()).id
+                    typeId = mapIdentToInst(fellesformat.msgHead.msgInfo.receiver.organisation.ident.first()).typeId
+
+                    val additionalIds = fellesformat.msgHead.msgInfo.receiver.organisation.ident.drop(1)
+                            .map { mapIdentToAdditionalId(it) }
+
+                    additionalId.addAll(additionalIds)
+                }
+            }
+
+            receiver.hcp.inst.apply {
+                name = fellesformat.msgHead.msgInfo.sender.organisation.organisationName
+
+                for (i in fellesformat.msgHead.msgInfo.sender.organisation.ident.indices) {
+                    id = mapIdentToInst(fellesformat.msgHead.msgInfo.sender.organisation.ident.first()).id
+                    typeId = mapIdentToInst(fellesformat.msgHead.msgInfo.sender.organisation.ident.first()).typeId
+
+                    val additionalIds = fellesformat.msgHead.msgInfo.sender.organisation.ident.drop(1)
+                            .map { mapIdentToAdditionalId(it) }
+
+                    additionalId.addAll(additionalIds)
+
+                }
+
+                hcPerson.apply {
+                    name = fellesformat.msgHead.msgInfo.sender.organisation.healthcareProfessional.givenName +
+                            fellesformat.msgHead.msgInfo.sender.organisation.healthcareProfessional.familyName
+
+                    for (i in fellesformat.msgHead.msgInfo.sender.organisation.healthcareProfessional.ident.indices) {
+                        id = mapIdentToInst(fellesformat.msgHead.msgInfo.sender.organisation.healthcareProfessional.ident.first()).id
+                        typeId = mapIdentToInst(fellesformat.msgHead.msgInfo.sender.organisation.healthcareProfessional.ident.first()).typeId
+
+                        val additionalIds = fellesformat.msgHead.msgInfo.sender.organisation.healthcareProfessional.ident.drop(1)
+                                .map { mapIdentToAdditionalId(it) }
+
+                        additionalId.addAll(additionalIds)
+                    }
+                }
+
+            }
+            status.apply {
+                v = "1"
+                dn = "OK"
+            }
+            originalMsgId.apply {
+                msgType.apply {
+                    v = "LE"
+                    dn = "Legeerklæring"
+                }
+                issueDate = fellesformat.msgHead.msgInfo.genDate
+                id = fellesformat.msgHead.msgInfo.msgId
+            }
+        }
+    }
+    return fellesformatApprec.toString()
+}
 
 
-    return "lool"
+
+fun mapIdentToAdditionalId(ident: Ident): AdditionalId = AdditionalId().apply {
+    id = ident.id
+    type = AppRecCS().apply {
+        dn = ident.typeId.dn
+        v = ident.typeId.v
+    }
+}
+
+fun mapIdentToInst(ident: Ident): Inst = Inst().apply {
+    id = ident.id
+     typeId= AppRecCS().apply {
+        dn = ident.typeId.dn
+        v = ident.typeId.v
+    }
 }
---
+++ src/main/kotlin/no/nav/legeerklaering/LegeerklaeringConstant.kt
@@ -0,0 +1,11 @@
+package no.nav.legeerklaering
+
+enum class LegeerklaeringConstant(val string: String) {
+    LE("LE"),
+    Legeerklæring("Legeerklæring"),
+    APPREC("APPREC"),
+    APPRECVersionV1_0(" 1.0 2004-11-21"),
+    ebRoleNav ("Nav"),
+    ebServiceLegemelding ("Legemelding"),
+    ebActionSvarmelding ("Svarmelding")
+}
