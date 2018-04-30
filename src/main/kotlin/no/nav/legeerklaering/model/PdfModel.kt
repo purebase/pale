0f08625addf6f9265784f6ed65ecceef014a772d
Kevin Sillerud <kevin.sillerud@gmail.com>
2018-04-30 16:51:47 +0200

    Start adding values to the fagmelding template

--- src/main/kotlin/no/nav/legeerklaering/model/PdfModel.kt
+++ src/main/kotlin/no/nav/legeerklaering/model/PdfModel.kt
@@ -28,6 +28,13 @@ data class BehandlingsvedleggSender(
 )
 
 data class Fagmelding(
+        val arbeidsvurderingVedSykefravaer: Boolean,
+        val arbeidsavklaringsPenger: Boolean,
+        val ufoerepensjon: Boolean,
+        val pasient: Pasient,
+        val plan: Plan,
+        val sykdomsOpplysninger: SykdomsOpplysninger,
+        val funksjonsOgArbeidsevne: FunksjonsOgArbeidsevne,
         val merknadAvvist: List<Merknad>,
         val merknadManuellBehandling: List<Merknad>,
         val merknadOppfoelging: List<Merknad>,
@@ -35,14 +42,58 @@ data class Fagmelding(
         val documentInfo: List<Merknad>
 )
 
-data class DocumentInfo(
-        val type: String,
-        val status: String,
-        val pasientId: String
+data class Pasient(
+        val fornavn: String,
+        val etternavn: String,
+        val foedselsnummer: String,
+        val navKontor: String,
+        val adresse: String,
+        val postnummer: Int,
+        val poststed: String,
+        val yrke: String
 )
 
-data class Sporing(
-        val ediLoggId: String
+data class SykdomsOpplysninger (
+    val hoveddiagnose: Diagnose,
+    val bidiagnose: List<Diagnose>,
+    val arbeidsufoerFra: String,
+    val sykdomsHistorie: String,
+    val statusPresens: String,
+    val boerNavKontoretVurdereOmDetErEnYrkesskade: Boolean
+)
+
+data class Diagnose(
+        val tekst: String,
+        val koder: List<String>
+)
+
+data class Plan(
+        val utredning: Henvisning,
+        val behandling: Henvisning,
+        val utredningsplan: String,
+        val behandlingsplan: String,
+        val vurderingAvTidligerePlan: String,
+        val naarSpoerreOmNyeLegeopplysninger: String,
+        val videreBehandlingIkkeAktuellGrunn: String
+)
+
+data class Henvisning(
+        val tekst: String,
+        val dato: LocalDateTime,
+        val antattVentetIUker: Int
+)
+
+data class FunksjonsOgArbeidsevne(
+        val behov: Boolean,
+        val kjoepAvHelsetjenester: Boolean,
+        val reisetilskudd: Boolean,
+        val aktivSykMelding: Boolean,
+        val hjelpemidlerArbeidsplassen: Boolean,
+        val arbeidsavklaringsPenger: Boolean,
+        val friskemeldingTilArbeidsformidling: Boolean,
+        val andreTiltak: String,
+        val naermereOpplysninger: String,
+        val tekst: String
 )
 
 data class Merknad(
