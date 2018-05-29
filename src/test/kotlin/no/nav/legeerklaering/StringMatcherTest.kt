f07811d1febb9c315799e622e06865012be5ed88
Kevin Sillerud <kevin.sillerud@gmail.com>
2018-05-29 08:44:58 +0200

    Forgot to add all files

--- src/test/kotlin/no/nav/legeerklaering/StringMatcherTest.kt
+++ src/test/kotlin/no/nav/legeerklaering/StringMatcherTest.kt
@@ -1,4 +1,28 @@
 package no.nav.legeerklaering
 
+import org.junit.Assert.*
+import org.junit.Test
+
 class StringMatcherTest {
+
+    @Test
+    fun validateDifferentStringsLessThen100() {
+        val percentageMatch = calculatePercentageStringMatch("abcdef", "abbdef")
+        println(percentageMatch)
+        assertTrue(percentageMatch < 1.0)
+    }
+
+    @Test
+    fun validateTwoSimilarNamesHasMoreThen70() {
+        val percentageMatch = calculatePercentageStringMatch("Oslo Legekontor AS", "Oslo Legekontor")
+        println(percentageMatch)
+        assertTrue(percentageMatch > 0.7)
+    }
+
+    @Test
+    fun validateTwoDifferentNamesHasLessThen70() {
+        val percentageMatch = calculatePercentageStringMatch("Bergen Legekontor", "Oslo Legekontor")
+        println(percentageMatch)
+        assertFalse(percentageMatch > 0.7)
+    }
 }
