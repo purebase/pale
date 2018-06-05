bf47858c7420fb08e7a5d2d83d0f0ae93773743c
Joakim kartveit <joakim.kartveit@nav.no>
2018-06-05 17:57:52 +0200

    Endret til ISO-8859-1 da meldingene kommer inn på dette formatet

--- src/test/kotlin/no/nav/pale/validation/PreTPSRuleFlowTest.kt
+++ src/test/kotlin/no/nav/pale/validation/PreTPSRuleFlowTest.kt
@@ -1,9 +1,13 @@
 package no.nav.pale.validation
 
+import no.nav.model.fellesformat.EIFellesformat
+import no.nav.pale.fellesformatJaxBContext
+import no.nav.pale.getResource
 import no.nav.pale.readToFellesformat
 import org.junit.Assert
 import org.junit.Assert.assertEquals
 import org.junit.Test
+import java.io.StringReader
 
 class PreTPSRuleFlowTest {
 
