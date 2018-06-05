1b9b7dd72b75259bd4ba0058995c5604c7c1c972
Joakim kartveit <joakim.kartveit@nav.no>
2018-06-05 16:19:13 +0200

    Logger meldingen, som kommer fra input melding fra kø

--- src/main/kotlin/no/nav/pale/PaleApplication.kt
+++ src/main/kotlin/no/nav/pale/PaleApplication.kt
@@ -155,6 +155,8 @@ fun listen(pdfClient: PdfClient, jedis: Jedis, personV3: PersonV3, organisasjonE
                 else -> throw RuntimeException("Incoming message needs to be a byte message or text message")
             }
 
+            log.info("Incomming messsage:" + inputMessageText)
+
             val fellesformat = fellesformatJaxBContext.createUnmarshaller().unmarshal(StringReader(inputMessageText)) as EIFellesformat
             INCOMING_MESSAGE_COUNTER.inc()
             val inputHistogram = INPUT_MESSAGE_TIME.startTimer()
