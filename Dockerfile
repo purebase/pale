ecaaab17e2a0998c9497083b793a77431a0db473
Joakim kartveit <joakim.kartveit@nav.no>
2018-06-20 17:49:38 +0200

    Tok og la til joark endpoint ws som fasit variabel

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,5 @@
 FROM navikt/java:8
 COPY build/install/* /app
 ENV JAVA_OPTS="'-Dlogback.configurationFile=logback-remote.xml'"
-ENV JOURNALBEHANDLING_ENDPOINTURL="https://wasapp-t1.adeo.no/joark/Journalbehandling"
 #RUN keytool -genkey -keystore mq_keystore.p12 -storetype pkcs12 -keyalg RSA -keysize 4096 -validity 3650 -alias mqkey -dname "cn=Unknown, ou=Unknown, o=Unknown, c=Unknown" -storepass changeit -keypass changeit
 ENTRYPOINT ["/app/bin/pale"]
--- src/main/kotlin/no/nav/pale/FasitProperties.kt
+++ src/main/kotlin/no/nav/pale/FasitProperties.kt
@@ -18,7 +18,7 @@ data class FasitProperties(
     val paleBackoutQueueName: String = getEnvVar("PALE_BACKOUT_QUEUE_QUEUENAME"),
     val securityTokenServiceUrl: String = getEnvVar("SECURITYTOKENSERVICE_URL"),
     val receiptQueueName: String = getEnvVar("MOTTAK_QUEUE_UTSENDING_QUEUENAME"),
-    val journalbehandlingEndpointURL: String = getEnvVar("JOURNALBEHANDLING_ENDPOINTURL"),
+    val journalbehandlingEndpointURL: String = getEnvVar("JOARK_JOURNALBEHANDLING_WS_ENDPOINT_URL"),
     val appName: String = getEnvVar("APP_NAME"),
     val appVersion: String = getEnvVar("APP_VERSION")
 )
