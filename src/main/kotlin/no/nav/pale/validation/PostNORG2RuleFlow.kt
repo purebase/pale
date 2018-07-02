e07410ccfca0c776748470105dee3197adb29589
Joakim kartveit <joakim.kartveit@nav.no>
2018-07-02 13:07:25 +0200

    La til at navkontor kan være null

--- src/main/kotlin/no/nav/pale/validation/PostNORG2RuleFlow.kt
+++ src/main/kotlin/no/nav/pale/validation/PostNORG2RuleFlow.kt
@@ -3,9 +3,9 @@ package no.nav.pale.validation
 import no.nav.pale.mapping.ApprecError
 import no.nav.tjeneste.virksomhet.organisasjonenhet.v2.informasjon.Organisasjonsenhet
 
-fun postNORG2Flow(navKontor: Organisasjonsenhet): List<Outcome> {
+fun postNORG2Flow(navKontor: Organisasjonsenhet?): List<Outcome> {
     val outcome = mutableListOf<Outcome>()
-    if (navKontor.enhetId == null || navKontor.enhetId.isEmpty()) {
+    if (navKontor?.enhetId == null || navKontor.enhetId.isEmpty()) {
         outcome += OutcomeType.PERSON_HAS_NO_NAV_KONTOR.toOutcome(apprecError = ApprecError.MISSING_PATIENT_INFO)
     }
 
